de8daeef16d2573d5d04d35ecbbe6c0835371d54
ChaseMayekar <vietchase@aol.com>
2017-04-21 11:20:59 -0400

    Add files via upload

---
+++ MainMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	int i = 2;
+
+	public void LoadbyIndex(int sceneIndex){
+		SceneManager.LoadScene (sceneIndex);
+	}
+
+	public void NextLevel(){
+		SceneManager.LoadScene (i);
+		i = i + 1;
+	}
+
+	public void ExitGame(){
+		Application.Quit();
+	}
+
+}
+	
