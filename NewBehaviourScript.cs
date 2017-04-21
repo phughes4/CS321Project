602a43831efa86d4b9f86159d041a90fdfe669c0
ChaseMayekar <vietchase@aol.com>
2017-04-21 11:06:41 -0400

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public void LoadbyIndex(int sceneIndex){
+		SceneManager.LoadScene (sceneIndex);
+	}
+
+	public void ExitGame(){
+		Application.Quit();
+	}
+
+}
+	
