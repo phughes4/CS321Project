ab36bcb78ad8fe9457233fddd93380937b34250c
Ryan Spence <ryaspence@gmail.com>
2017-04-27 11:25:58 -0400

    Added test code
    Unity threw a shitfit, so I can't test it myself.

--- BoardManager.cs
+++ BoardManager.cs
@@ -2,21 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Nunit.Framework;
+
+[TestFixture]
+public class BoardManagerTests
+{
+	[Test]
+	public void setGoalRed(bool x)
+	{
+		x = true;
+		goalRed = x;
+		Assert.IsTrue(goalRed);
+		x = false;
+		goalRed = x;
+		Assert.IsTrue(goalRed);
+	}
+	
+	[Test]
+	public void setGoalGreen(bool x)
+	{
+		x = true;
+		goalGreen = x;
+		Assert.IsTrue(goalGreen);
+		x = false;
+		goalGreen = x;
+		Assert.IsTrue(goalGreen);
+	}
+	
+}
+
 
 public class BoardManager : MonoBehaviour {
 
     //Boardmanager acts like a repository, it keeps track of information while other programs are workhorses.
-    public int sceneName = 0;//This should let us change it in the Unity Browser.
+    //public int sceneName = 0;//This should let us change it in the Unity Browser.
     public bool goalRed = false;//public only to mess with it in scene editor.
     public bool goalBlue = false;//Can be changed to private later if need be.
     public bool goalGreen = false;
     public AudioClip win;
 
     //Setters for the stats. Other scripts should be able to access these to change values at any time.
+
     public void setGoalRed(bool x)
     { goalRed = x; }
    // public void setGoalBlue(bool x)
     //{ goalBlue = x; }
+
     public void setGoalGreen(bool x)
     { goalGreen = x; }
 
@@ -30,15 +61,11 @@ public class BoardManager : MonoBehaviour {
 	    //This stuff works as long as the scenes are registered as levels in the build. We will need to do this anyway in order to switch levels.
 	    //https://docs.unity3d.com/ScriptReference/Application.LoadLevel.html
 	
-	
-       // if(Input.GetKeyDown("p"))//Pause menu?
-       // {
-	        //Display the pause menu, or the controls, or something. Optional.
-       // }
+
         Debug.Log("green is "+goalGreen+". red is "+goalRed);
         if(goalRed == true && goalGreen == true)
         {
-            //Do the win menu. (GUI) Get from Chase.
+            //Do the win menu.
             AudioManager.instance.PlaySingle(win, 1f);
             Application.LoadLevel(1);
 	        //For now, we'll just flatly load the next level.
