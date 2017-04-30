4ccc72afb86361c5802636ec02562cf17364bd21
ChaseMayekar <vietchase@aol.com>
2017-04-30 15:52:25 -0400

    Add files via upload

---
+++ UiTestingi.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiTestingi : MonoBehaviour {
+
+	public void MainMenuTest(){
+
+		int test = 2;
+		MainMenuScript.Resetter ();
+
+		Assert.AreEqual(test, MainMenuScript.i)
+
+		MainMenuScript.NextLevel ();
+
+		test = 3;
+
+		Assert.AreEqual(test, MainMenuScript.i)
+
+		MainMenuScript.Resetter ();
+
+		Assert.AreEqual(test, MainMenuScript.i)
+	}
+}
